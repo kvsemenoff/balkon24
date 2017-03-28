6483b7da7ddb8f12533a181efe6024ed2c24204e
roshett <roshette@yandex.ru>
2017-03-28 17:46:05 +0300

    page.php-fix-6

---
+++ wp-content/themes/balkon/template_price_glazing.php
@@ -0,0 +1,423 @@
+<?php 
+/*
+Template Name: Остекление балконов
+*/
+?>
+<?php get_header(); ?>
+<div class="dd-menu-mobile">
+	<div class="js-but">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/burg.png" alt="">
+	</div>
+</div>
+<?php get_template_part( 'breadcrumbs' );  ?>
+
+<section>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12">
+				<div class="wrap-cold-h1 wrap-cold-h1_mt wrap-cold-h1_mb">
+					<h1>Цена остекленения балконов и лоджий</h1>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+
+			<div class="col-md-6">
+				<div class="dd-box-wrap">
+					<div class="dd-title_vid">
+						<div class="dd-title_view">
+							<span>Холодное остекленение</span>
+						</div>
+					</div>
+
+					<div class="dd-img_vid float_vid">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/b1.png" alt="">
+					</div>
+
+					<div class="dd-right_vid float_vid">
+						<div class="dd-txt_vid">
+							<span>Выгодная стоимость</span><br>
+							<span>Удобно для небольшого<br> балкона</span><br>
+							<span class="mb_none_vid">Долгий срок службы</span><br>
+							<span class="dd-sp4_vid">от<b> 12860 </b>руб.</span>
+						</div>
+						<div class="dd-but_txt">
+							<a class="button-my" href="#">Подробнее</a>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<div class="dd-box-wrap">
+					<div class="dd-title_vid mt2">
+						<div class="dd-title_view">
+							<span>Теплое остекленение</span>
+						</div>
+					</div>
+
+					<div class="dd-img_vid float_vid">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/b2.png" alt="">
+					</div>
+
+					<div class="dd-right_vid float_vid">
+						<div class="dd-txt_vid">
+							<span>На балконе всегда тепло</span><br>
+							<span>Самая лучшая защита от шума</span><br>
+							<span class="mb_none_vid">Можно объеденить балкон с<br> 
+								комнатой</span><br>
+								<span class="dd-sp4_vid">от<b> 21400 </b>руб.</span>
+							</div>
+							<div class="dd-but_txt">
+								<a class="button-my" href="#">Подробнее</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<div class="col-md-12">
+
+					<div class="dd-txt_top_pg">
+						<p>У нас без дополнительных платежей! Озвученные при расчете суммы закрепляются в договоре без последующих изменений.
+						</p>
+						<span>
+							Кто-то желает превратить балкон в теплую и комфортабельную комнату, кто-то использует его в качестве технического или хозяйственного помещения, а кому-то достаточно, чтобы внутрь лоджии просто не попадали осадки, пыль и грязь.
+						</span>
+					</div>
+
+				</div>
+				<div class="clearfix"></div>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section class="dd-wrap_pr">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12">
+					<div class=" wrap-cold-h1__2">
+						<h2>Расчет стоимость по серии дома</h2>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/pr1.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Лоджия: 121 серия</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>23000</strike> руб. <br><b>17000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>24500</strike> руб. <br><b>19000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/pr2.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="dd-ll_2"></div>
+				<!-- /////////////////////////// -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/pr3.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Балкон: 121 серия</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>29000</strike> руб. <br><b>21000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>32500</strike> руб. <br><b>24000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/pr4.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<div class="dd-ll"></div>
+				</div>
+				<!-- //////////////////////////////// -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/pr5.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Балкон: Хрущевка</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>45000</strike> руб. <br><b>33000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>47600</strike> руб. <br><b>35000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/pr6.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<div class="dd-ll"></div>
+				</div>
+				<!-- /////////////////////////////// -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/pr7.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Балкон: Брежневка</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>45000</strike> руб. <br><b>33000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>47600</strike> руб. <br><b>35000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/pr6.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<div class="dd-ll"></div>
+				</div>
+				<!-- //////////////////////////////////////////// -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/p8.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Лоджия: 97 серия</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>29000</strike> руб. <br><b>21000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>31500</strike> руб. <br><b>24000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/p9.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<div class="dd-ll"></div>
+				</div>
+				<!-- ///////////////////////////// -->
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-img-box_pr">
+						<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/p10.png" alt="">
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-min-box_h dd-min-box_h__2">
+						<span class="dd-sp1_pr">Балкон: 97 серия</span>
+						<div class="dd-bottom-box_h dd-bottom-box_h__pr">
+							<span class="dd-sp2_pr">Холодное <br>
+								остекленение
+							</span>
+							<span> <strike>39500</strike> руб. <br><b>29000</b> руб.</span><br>
+
+							<span class="dd-sp2_pr">Теплое <br>
+								остекленение
+							</span>
+							<span><strike>43500</strike> руб. <br><b>32000</b> руб.</span><br>
+							<a class="button-my" href="#">Заказать со скидкой</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-sm-4">
+					<div class="dd-pr_img">	
+						<img src="<?php echo get_template_directory_uri(); ?>/img/p11.png" alt="">
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+
+				<!-- ///////////////////////////////////// -->
+
+			</div>
+		</div>
+	</section>
+
+	<section class="dd-wrap_zp">
+		<div class="container poz-rel_zp">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="dd-but_zp">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/vniz.png" alt="">
+					</div>
+					<div class="dd-box_zp">
+						<span>Если вы не нашли свой балкон, не расстраивайтесь</span>
+						<span>Звоните по телефону <b>+7 (351) 555-33-22</b></span>
+						<span>и мы выполним точный расчет!</span>
+					</div>
+				</div>						
+			</div>
+		</div>								
+	</section>
+
+	<section class="section section_padding dfpadding dd-dfpadding"> 
+		<div class="container">
+			<div class="dfwarmblock">
+				<div class="dfwarmbox">
+					<div class="dfwarmimmg"><img src="<?php echo get_template_directory_uri(); ?>/img/gg1.png" alt=""></div>
+					<h4 class="dfwarmcap"><span>Отделка под ключ</span></h4>
+					<p class="dfwarmtxtbot dfwarmtxtbot_dd">После остекленения балкона или лоджии на повестку дня встает вопрос об их отделке. 
+					</p>
+					<a href="#" class="dfbuttonwarm">Подробнее</a>
+				</div>
+				<div class="dfwarmbox">
+					<div class="dfwarmimmg"><img src="<?php echo get_template_directory_uri(); ?>/img/gg2.png" alt=""></div>
+					<h4 class="dfwarmcap"><span>Последние работы</span></h4>
+					<p class="dfwarmtxtbot dfwarmtxtbot_dd">После остекленения балкона или лоджии на повестку дня встает вопрос об их отделке. 
+					</p>	
+					<a href="#" class="dfbuttonwarm">Подробнее</a>		
+				</div>
+				<div class="dfwarmbox">
+					<div class="dfwarmimmg"><img src="<?php echo get_template_directory_uri(); ?>/img/gg3.png" alt=""></div>
+					<h4 class="dfwarmcap"><span>Калькулятор</span></h4>
+					<p class="dfwarmtxtbot dfwarmtxtbot_dd">Балконный калькулятор позволяет на предварительном этапе рассчитать стоимость остекления балкона...
+					</p>
+					<a href="#" class="dfbuttonwarm">Рассчитать</a>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+	<section class="dd-box-txt_lp_2">
+		<div class="container poz-rel_zp">
+			<div class="dd-but_zp">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/vniz2.png" alt="">
+			</div>
+			<div class="dd-box-txt_lp ">
+				<p>Можно с легкостью найти множество причин для того, чтобы произвести остекление балкона. Это не только эстетическая красота, но и практичность остекленного балкона. Ваш балкон будет защищен от уличного шума, грязи и пыли, а также будет сохранять тепло внутри дома.
+				</p>
+				<div class="dd-box-grey_lp">
+					<p>
+						Все эти функции будут выполняться вашим балконом только в том случае, если остекление производиться профессионалами, а качество используемых алюминиевых или пластиковых конструкций отвечает самым высоким требованиям. Не стоит сразу кидаться на те предложения и объявления, которые обещают слишком низкую стоимость остекления балконов. <br><br>
+
+						В нашей компании вы сможете заказать действительно качественный балкон под ключ. Наши специалисты проведут остекление из алюминия или ПВХ, а также утеплят балкон и займутся его внутренней и внешней обшивкой.
+					</p>
+				</div>	
+			</div>
+		</div>
+	</section>
+
+	<section class="section section_padding section_db-zamer_view section_db-zamer_view_dd">
+		<div class=" container">
+			<div class="row">
+				<div class="col-md-12">
+					<p class="db-zamer_p-one">запишитесь на бесплатный замер<br>
+						<span>и получите 2000 руб!</span>
+					</p>
+					<p class="db-zamer_p-two">
+						До 18 января получите дополнительную скидку 2 000 руб. при выезде замерщика. Просто заполните форму ниже!
+					</p>
+				</div>
+				<form action="#" method="post" class="db-zamer-form">
+					<div class="col-md-4">
+						<input type="text" placeholder="Имя" name="uname">
+					</div>
+					<div class="col-md-4">
+						<input type="text" placeholder="Телефон" name="phone">
+					</div>
+					<div class="col-md-4">
+						<input type="submit" value="перезвоните мне" name="submit">
+					</div>
+				</form>
+				<div class="clearfix"></div>
+				<div class="col-md-12">
+					<p class="db-zamer_p-three">И мы скоро вам перезвоним<br>
+						<span>Сегодня записались 12 человек, последний 16 минут назад</span>
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<?php get_footer(); ?>
