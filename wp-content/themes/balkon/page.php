f0bce51183a453f68e06c710047b64cfee992815
roshett <roshette@yandex.ru>
2017-03-28 16:48:53 +0300

    page.php-fix-3

--- wp-content/themes/balkon/page.php
+++ wp-content/themes/balkon/page.php
@@ -38,7 +38,11 @@
 <section class="section section_padding dfpadding"> 
 	<div class="container">
 		<h1 class="db-our-h1"><?php the_title(); ?></h1>
-		<div class="dftxtbottom dfwarmpd"><?php the_content(); ?></div>
+		<div class="dftxtbottom dfwarmpd">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<?php the_content(); ?>
+		<?php endwhile; ?>
+		</div>
 	</div>
 </section>
 
