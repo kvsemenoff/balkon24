c076372744dcfb0ade592e36bb424fbde68d2b1e
roshett <roshette@yandex.ru>
2017-03-28 12:16:26 +0300

    fucntions.php

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -5638,3 +5638,177 @@ function wp_cache_get_last_changed( $group ) {
 
 	return $last_changed;
 }
+
+// My work;
+// Отзывы
+add_action('init', 'testimonials_register');
+function testimonials_register() {
+    $args = array(
+        'label'               => __('Отзывы'),
+        'labels'              => array(
+            'name'               => __('Отзывы'),
+            'singular_name'      => __('Отзывы'),
+            'menu_name'          => __('Отзывы'),
+            'all_items'          => __('Все отзывы'),
+            'add_new'            => _x('Добавить отзыв', 'product'),
+            'add_new_item'       => __('Новый отзыв'),
+            'edit_item'          => __('Редактировать отзыв'),
+            'new_item'           => __('Новый отзыв'),
+            'view_item'          => __('Отзывы'),
+            'not_found'          => __('Отзыв не найден'),
+            'not_found_in_trash' => __('Удаленных отзывов нет'),
+            'search_items'       => __('Найти отзыв')
+        ),
+        'description'         => __('Отзывы'),
+        'public'              => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'show_in_nav_menus'   => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'capability_type'     => 'post',
+        'hierarchical'        => false,
+        'supports'            => array(
+            'title'
+            
+   
+        ),
+        'has_archive'         => false,
+        'rewrite'             => array(
+            'slug'       => '',
+            'with_front' => false
+        )
+    );
+    register_post_type('testimonials', $args);
+}
+// акции
+add_action('init', 'events_register');
+function events_register() {
+    $args = array(
+        'label'               => __('Акции'),
+        'labels'              => array(
+            'name'               => __('Акции'),
+            'singular_name'      => __('Акции'),
+            'menu_name'          => __('Акции'),
+            'all_items'          => __('Все акции'),
+            'add_new'            => _x('Добавить акцию', 'product'),
+            'add_new_item'       => __('Новая акция'),
+            'edit_item'          => __('Редактировать акцию'),
+            'new_item'           => __('Новая акция'),
+            'view_item'          => __('Акции'),
+            'not_found'          => __('Акция не найдена'),
+            'not_found_in_trash' => __('Удаленных акций нет'),
+            'search_items'       => __('Найти акцию')
+        ),
+        'description'         => __('Акции'),
+        'public'              => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'show_in_nav_menus'   => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'capability_type'     => 'post',
+        'hierarchical'        => false,
+        'supports'            => array(
+            'title'
+            
+   
+        ),
+        'has_archive'         => false,
+        'rewrite'             => array(
+            'slug'       => '',
+            'with_front' => false
+        )
+    );
+    register_post_type('events', $args);
+}
+// Статьи
+add_action('init', 'articles_register');
+function articles_register() {
+    $args = array(
+        'label'               => __('Статьи'),
+        'labels'              => array(
+            'name'               => __('Статьи'),
+            'singular_name'      => __('Статьи'),
+            'menu_name'          => __('Статьи'),
+            'all_items'          => __('Все статьи'),
+            'add_new'            => _x('Добавить статью', 'product'),
+            'add_new_item'       => __('Новая статья'),
+            'edit_item'          => __('Редактировать статью'),
+            'new_item'           => __('Новая статья'),
+            'view_item'          => __('Статьи'),
+            'not_found'          => __('Статья не найдена'),
+            'not_found_in_trash' => __('Удаленных статей нет'),
+            'search_items'       => __('Найти статью')
+        ),
+        'description'         => __('Статьи'),
+        'public'              => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'show_in_nav_menus'   => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'capability_type'     => 'post',
+        'hierarchical'        => false,
+        'supports'            => array(
+            'title'
+            
+   
+        ),
+        'has_archive'         => false,
+        'rewrite'             => array(
+            'slug'       => '',
+            'with_front' => false
+        )
+    );
+    register_post_type('articles', $args);
+}
+//Работа
+add_action('init', 'works_register');
+function works_register() {
+    $args = array(
+        'label'               => __('Работа'),
+        'labels'              => array(
+            'name'               => __('Работа'),
+            'singular_name'      => __('Работа'),
+            'menu_name'          => __('Работа'),
+            'all_items'          => __('Все работы'),
+            'add_new'            => _x('Добавить работу', 'product'),
+            'add_new_item'       => __('Новая работа'),
+            'edit_item'          => __('Редактировать работу'),
+            'new_item'           => __('Новая работа'),
+            'view_item'          => __('Работы'),
+            'not_found'          => __('Работа не найдена'),
+            'not_found_in_trash' => __('Удаленных работ нет'),
+            'search_items'       => __('Найти работу')
+        ),
+        'description'         => __('Работа'),
+        'public'              => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'show_ui'             => true,
+        'show_in_nav_menus'   => true,
+        'show_in_menu'        => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'capability_type'     => 'post',
+        'hierarchical'        => false,
+        'supports'            => array(
+            'title'
+            
+   
+        ),
+        'has_archive'         => false,
+        'rewrite'             => array(
+            'slug'       => '',
+            'with_front' => false
+        )
+    );
+    register_post_type('works', $args);
+}
