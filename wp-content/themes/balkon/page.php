a7eecedfb15eb170a27e926027582da78f4ad66b
roshett <roshette@yandex.ru>
2017-03-28 16:55:59 +0300

    page.php-fix-4

--- wp-content/themes/balkon/page.php
+++ wp-content/themes/balkon/page.php
@@ -1,40 +1,6 @@
 <?php get_header(); ?>
+<?php get_template_part( 'breadcrumbs' );  ?>
 
-<div class="dd-bg_grey">
-	<section class="section section_padding">
-	<div class=" container">
-		<div class="row">
-
-			<div class="col-md-8">
-				
-				<div class="dd-brad-cramp dd-pad_top">
-					<a href="#">Главная</a>
-					<span class="dd-str_bc"></span>
-					<a href="#">Тест</a>
-					<span class="dd-str_bc"></span>
-					<span>Конечная страница</span>
-				</div>
-
-			</div>
-
-			<div class="col-md-4">
-
-				<div class="dd-calc dd-pad_top">
-					<div class="dd-wr_calc">
-						<a href="#">Калькулятор</a>
-					</div>
-					<div class="dd-wr_calc dd-wr_calc_2">
-						<a href="#">Вызов замерщика</a>
-					</div>
-				</div>
-
-			</div>
-			<div class="clearfix"></div>
-
-		</div>
-	</div>
-</section>
-</div>
 <section class="section section_padding dfpadding"> 
 	<div class="container">
 		<h1 class="db-our-h1"><?php the_title(); ?></h1>
