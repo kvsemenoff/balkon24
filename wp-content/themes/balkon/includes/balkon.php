284dccccc4b514502732b368e17ec9ec5571e1c6
roshett <roshette@yandex.ru>
2017-03-28 15:10:46 +0300

    fucntions.php

---
+++ wp-content/themes/balkon/includes/balkon.php
@@ -0,0 +1,262 @@
+<section class="dfbgcolor">
+	<div class="container">
+			<h2 class="dfdectopcap dfcapsl">Отделка и утепление балкона</h2>
+			<div class="dftxtbottom dfsltop">Кто-то желает превратить балкон в теплую и комфортабельную комнату, кто-то использует его в качестве технического или хозяйственного помещения, а кому-то достаточно, чтобы внутрь лоджии просто не попадали осадки, пыль и грязь.</div>
+
+			<div class="dfslider" id="dfbalk1">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider1.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider2.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk2">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider2.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider2.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk3">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider3.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider3.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk4">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider5.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider5.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk5">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider5.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider5.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk6">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider6.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider6.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<div class="dfslider" id="dfbalk7">
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider7.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="dfimgtext">
+						<div class="dfimgsl">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/dfslider7.jpg" alt="">
+						</div>
+						<div class="dftextleft">
+							<h4>Установка <br> <span>освещения</span></h4>
+							<p>На данный момент этот вид отделки является одним из наиболее экономичных. Также обшивка пластиковыми панелями применяется для выравнивания стен или потолка.</p>
+							<span class="dfslprice">от
+								<strong>1400</strong>
+								руб.		
+							</span>
+							<a href="#" class="dfsllink">Подробнее</a>
+						</div>
+					</div>
+				</div>
+			</div>	
+
+
+			<div class="dfnavtab dfbalkon" >
+				<ul>
+					<li><a href="#dfbalk1" class="dfnavlink dfactivelink"><b>Ламинированные панели   </b> </a></li>
+					<li><a href="#dfbalk2" class="dfnavlink"><b>Пластиковые панели</b></a></li>
+					<li><a href="#dfbalk3" class="dfnavlink"><b>Вагонка</b></a></li>
+					<li><a href="#dfbalk4" class="dfnavlink"><b>МДФ</b></a></li>
+					<li><a href="#dfbalk5" class="dfnavlink"><b>Камень</b></a></li>
+					<li><a href="#dfbalk6" class="dfnavlink"><b>Кирпич</b></a></li>
+					<li><a href="#dfbalk7" class="dfnavlink"><b>Штукатурка </b></a></li>
+					<li><a href="#dfbalk7" class="dfnavlink"><b> Утепление</b></a></li>
+				</ul>
+			</div>
+			<div class="dflinksliderbot">
+				<a href="#">заказать отделку под ключ</a>
+			</div>			
+	</div>
+</section>	
