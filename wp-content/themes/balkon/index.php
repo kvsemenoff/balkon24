46f84114bf065f8a4a3d86495408db12774f3557
roshett <roshette@yandex.ru>
2017-03-28 14:59:17 +0300

    fucntions.php

--- wp-content/themes/balkon/index.php
+++ wp-content/themes/balkon/index.php
@@ -1,17 +1,18 @@
 <!-- <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header-top.php'); ?>
-<?php require_once('includes/select.php'); ?>
-<?php require_once('includes/our.php'); ?>
-<?php require_once('includes/we.php'); ?>
-<?php require_once('includes/other.php'); ?>
-<?php require_once('includes/balkon.php'); ?>
-<?php require_once('includes/zamer.php'); ?>
-<?php require_once('includes/kachestvo.php'); ?>
+
 <?php require_once('includes/footer.php'); ?>
 <?php require_once('includes/footer.php'); ?>
  -->
  <?php get_header(); ?>
  <?php get_header_top(); ?>
+ <?php require_once('includes/select.php'); ?>
+ <?php require_once('includes/our.php'); ?>
+ <?php require_once('includes/we.php'); ?>
+ <?php require_once('includes/other.php'); ?>
+ <?php require_once('includes/balkon.php'); ?>
+ <?php require_once('includes/zamer.php'); ?>
+ <?php require_once('includes/kachestvo.php'); ?>
  <?php get_footer(); ?>
 </body>
 </html>
